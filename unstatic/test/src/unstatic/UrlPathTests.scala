bbc0a1711790eeece08ef9c2e6b7dddf5255f80f
Steve Waldman <swaldman@mchange.com>
2023-01-22 13:21:05 -0500

    Update test to track changes in main lib.

--- unstatic/test/src/unstatic/UrlPathTests.scala
+++ unstatic/test/src/unstatic/UrlPathTests.scala
@@ -4,18 +4,18 @@ import utest.*
 
 object UrlPathTests extends TestSuite:
   val BaseUrl = UrlPath.Abs("https://www.mchange.com/")
-  val HelloThereUnrooted = UrlPath.Unrooted.fromElements("hello", "there")
-  val HelloThere = BaseUrl.resolve(HelloThereUnrooted)
-  val HelloAgain = HelloThere.resolveSibling(UrlPath.Unrooted("again"))
+  val HelloThereRel = UrlPath.Rel.fromElements("hello", "there")
+  val HelloThere = BaseUrl.resolve(HelloThereRel)
+  val HelloAgain = HelloThere.resolveSibling(UrlPath.Rel("again"))
 
   val tests = Tests {
     test("UrlPath.Abs.resolve") {
       assert(HelloThere == UrlPath.Abs("https://www.mchange.com/hello/there"))
     }
     test("UrlPath.Abs.relativize") {
-      assert(BaseUrl.relativize(HelloThere) == HelloThereUnrooted)
+      assert(BaseUrl.relativize(HelloThere) == HelloThereRel)
     }
     test("UrlPath.Abs.resolveSibling") {
-      assert(HelloAgain == BaseUrl.resolve(UrlPath.Unrooted("hello/again")))
+      assert(HelloAgain == BaseUrl.resolve(UrlPath.Rel("hello/again")))
     }
   }
