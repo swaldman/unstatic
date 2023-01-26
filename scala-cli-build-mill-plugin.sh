f7cea1332126c3bcb4629d3da7b651d6da342102
Steve Waldman <swaldman@mchange.com>
2023-01-26 17:11:31 -0500

    Modify scala-cli unstatic-mill publishing script so that 'local' can easily be commented out.

--- scala-cli-build-mill-plugin.sh
+++ scala-cli-build-mill-plugin.sh
@@ -4,7 +4,9 @@ UNSTATIC_VERSION="0.0.1-SNAPSHOT"
 UNTEMPLATE_VERSION="0.0.2"
 MILL_VERSION="0.10.11"
 
-scala-cli publish local \
+scala-cli \
+    publish \
+    local \
     -Ytasty-reader \
     --version "0.0.1-SNAPSHOT" \
     --dep com.lihaoyi::mill-main:$MILL_VERSION \
