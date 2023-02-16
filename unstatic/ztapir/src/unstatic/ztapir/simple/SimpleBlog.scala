c0a577005702ac70bdca8823d8614438db538fe0
Steve Waldman <swaldman@mchange.com>
2023-02-16 01:41:58 -0500

    Work around problems with SortedSet maxBefore / minAfter.

--- unstatic/ztapir/src/unstatic/ztapir/simple/SimpleBlog.scala
+++ unstatic/ztapir/src/unstatic/ztapir/simple/SimpleBlog.scala
@@ -152,7 +152,7 @@ trait SimpleBlog extends ZTBlog:
    * Reverse-chronological!
    */
   given entryOrdering : Ordering[EntryResolved] =
-    Ordering.by( (er : EntryResolved) => (er.entryInfo.pubDate, er.entryUntemplate.UntemplatePackage, er.entryUntemplate.UntemplateName) ).reverse
+    Ordering.by( (er : EntryResolved) => (er.entryInfo.pubDate, er.entryUntemplate.UntemplateFullyQualifiedName) ).reverse
 
   val site                : Site // the type is Blog.this.Site, narrowed to ZTSite by ZTBlog
   val frontPage           : SiteLocation
@@ -218,6 +218,27 @@ trait SimpleBlog extends ZTBlog:
 
   def layoutPage( input : Layout.Input.Page ) : String
 
+  // WORKAROUND!!!
+  // temporary, until https://github.com/scala/bug/issues/12727 is fixed
+  // private lazy val tmpForwardSortedEntries = immutable.SortedSet.from( entriesResolved )( using Ordering.by( er => (er.entryInfo.pubDate, er.entryUntemplate.UntemplateFullyQualifiedName) ) )
+  private lazy val reverseChronoVec = entriesResolved.toVector
+  private lazy val resolvedToIndex = reverseChronoVec.zip(immutable.LazyList.from(0)).toMap
+
+  def previous( resolved : EntryResolved ) : Option[EntryResolved] =
+    // entriesResolved.minAfter(resolved) // broken, see https://github.com/scala/bug/issues/12727
+    // tmpForwardSortedEntries.maxBefore( resolved )
+    val index = resolvedToIndex(resolved)
+    val prevIndex = index + 1 // reverse ordered!
+    if reverseChronoVec.size > prevIndex then Some(reverseChronoVec(prevIndex)) else None
+
+  def next( resolved : EntryResolved ) : Option[EntryResolved] =
+    // entriesResolved.maxBefore(resolved) // broken, see https://github.com/scala/bug/issues/12727
+    // tmpForwardSortedEntries.minAfter( resolved )
+    val index = resolvedToIndex(resolved)
+    val nextIndex = index - 1 // reverse ordered!
+    if nextIndex >= 0 then Some(reverseChronoVec(nextIndex)) else None
+
+
   def renderSingleFragment( renderLocation : SiteLocation, resolved : EntryResolved, presentation : Entry.Presentation ) : String =
     val contentType = resolved.entryInfo.contentType
     val htmlifier = htmlifierForContentType(contentType).getOrElse {
@@ -228,7 +249,7 @@ trait SimpleBlog extends ZTBlog:
     val htmlifierOptions = Htmlifier.Options(generatorFullyQualifiedName = Some(resolved.entryUntemplate.UntemplateFullyQualifiedName))
     val htmlResult = htmlifier(result.text, htmlifierOptions)
     val info = resolved.entryInfo
-    val layoutEntryInput = Layout.Input.Entry(this, site, renderLocation, htmlResult, resolved, entriesResolved.maxBefore(resolved), entriesResolved.minAfter(resolved), presentation )
+    val layoutEntryInput = Layout.Input.Entry(this, site, renderLocation, htmlResult, resolved, previous(resolved), next(resolved), presentation )
     val hashSpecialsUnresolvedHtml = layoutEntry( layoutEntryInput )
     if entryFragmentsResolveHashSpecials then
       val resolveEscapes = // we only want to do this once for each piece of text
