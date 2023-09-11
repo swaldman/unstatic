a1b12e8e5cae7730b98b7115d625ba63552cfce8
Steve Waldman <swaldman@mchange.com>
2023-09-11 14:26:40 -0400

    Add a relativize method to PageBase, for when we are using paths without SiteLocations.

--- unstatic/src/unstatic/PageBase.scala
+++ unstatic/src/unstatic/PageBase.scala
@@ -8,4 +8,5 @@ object PageBase:
   def fromParent( siteRootedParentPath : Rooted )                : PageBase = PageBase(siteRootedParentPath)
   def fromParent( siteRootedParentLocation : Site#SiteLocation ) : PageBase = fromParent(siteRootedParentLocation.siteRootedPath)
   private def apply( siteRootedParentOfPage : Rooted )           : PageBase = new PageBase(siteRootedParentOfPage)
-case class PageBase( siteRootedParentOfPage : Rooted )
+case class PageBase( siteRootedParentOfPage : Rooted ):
+  def relativize( siteRootedPath : Rooted ) : Rel = this.siteRootedParentOfPage.relativize(siteRootedPath)
