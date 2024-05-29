aa18824ad24bd2d701d43e89098bcf84278f20ae
Steve Waldman <swaldman@mchange.com>
2024-05-29 12:03:32 -0400

    Implement strict parsing of untemplate attributes when found, informative-ish exceptions when found but not parsable.

--- ztapir/src/unstatic/ztapir/simple/Attribute.scala
+++ ztapir/src/unstatic/ztapir/simple/Attribute.scala
@@ -23,53 +23,57 @@ object Attribute:
       def checkAttributeKey[T]( key : Attribute.Key[T] ) : Option[T] = key.caseInsensitiveCheck( resolved.entryUntemplate )
 
     object Converter:
+      private def unexpectedType(key : String, expected : String, found : Any) : Nothing =
+        throw new BadAttributeException( s"${key}: Unexpected type. Expected ${expected}. Found: ${found}" )
       val SimpleString : Converter[String] =
-        (a : Any) =>
+        (key : String, a : Any) =>
           a match
-            case s : String => Some(s.trim)
-            case _          => None
+            case s : String => s.trim
+            case other      => unexpectedType(key, "String", other)
       val StringList : Converter[List[String]] =
-        (a : Any) =>
+        (key : String, a : Any) =>
           a match
-            case s : String => Some(s.split(",").map(_.trim).toList)
-            case _          => None
+            case list : List[String] @ unchecked =>
+              if list.forall( _.isInstanceOf[String] ) then // gotta check at runtime
+                list
+              else
+                throw new BadAttributeException( s"${key}: At least one element is not a String: ${list}" )
+            case s : String       => s.split(",").map(_.trim).toList
+            case other            => unexpectedType(key, "List[String] or comma-delimited String", other)
       val Timestamp : Converter[Instant] =
-        (a : Any) =>
+        (key : String, a : Any) =>
           a match
-            case s : String  => Some( parseTimestamp(s).get )
-            case i : Instant => Some(i)
-            case _           => None
+            case s : String  => parseTimestamp(s).recover( t => throw new BadAttributeException( s"${key}: '$s' could not be parsed as a timestamp.", t) ).get
+            case i : Instant => i
+            case other       => unexpectedType(key, "Instant or timestamp as String", other)
       val SimpleBoolean : Converter[Boolean] =
-        (a : Any) =>
+        (key : String, a : Any) =>
           a match
-            case b : Boolean => Some(b)
-            case s : String  => Try( s.toBoolean ).toOption
-            case _           => None
+            case b : Boolean => b
+            case s : String  => Try( s.toBoolean ).recover( t => throw new BadAttributeException( s"${key}: '$s' could not be coerced to a Boolean value as required.", t) ).get
+            case other       => unexpectedType(key, "Boolean or boolean value as String", other)
       val UpdateRecords : Converter[immutable.SortedSet[UpdateRecord]] =
-        (a : Any) =>
+        (key : String, a : Any) =>
           a match
             case io : IterableOnce[UpdateRecord] @unchecked => // have to check at runtime
-              val out = immutable.SortedSet.from(io)
-              if out.forall( _.isInstanceOf[UpdateRecord] ) then Some(out) else None
-            case _ => None
-    type Converter[T] = Any => Option[T]
+              val list = io.iterator.to(List)
+              if list.forall( _.isInstanceOf[UpdateRecord] ) then
+                immutable.SortedSet.from(list)
+              else
+                throw new BadAttributeException( s"${key}: At least one element is not an UpdateRecord: ${io}" )
+            case other => unexpectedType(key, "an iterable collection of UpdateRecord", other)
+    type Converter[T] = (String, Any) => T
     abstract class Abstract[T](val converter : Key.Converter[T], val variations : List[String]):
-      lazy val allNames = (this.toString :: this.variations)
-      lazy val lcs = allNames.map(LowerCased.apply)
-      def caseInsensitiveCheck(ut : Untemplate.AnyUntemplate) : Option[T] =
-        val lcMap = ut.UntemplateAttributesLowerCased
-        def find( list : List[LowerCased] ) : Option[T] =
+      private lazy val allNames = (this.toString :: this.variations)
+      private lazy val lcs = allNames.map(LowerCased.apply)
+      private def check[X]( map : immutable.Map[X,Any], allKeys : List[X] ) : Option[T] =
+        def find( list : List[X] ) : Option[T] =
           list match
-            case head :: tail => lcMap.get(head).flatMap(converter) orElse find(tail)
+            case head :: tail => map.get(head).map(value => converter(head.toString,value)) orElse find(tail)
             case Nil          => None
-        find(lcs)
-      def caseSensitiveCheck(ut : Untemplate.AnyUntemplate) : Option[T] =
-          val map = ut.UntemplateAttributes
-          def find( list : List[String] ) : Option[T] =
-            list match
-              case head :: tail => map.get(head).flatMap(converter) orElse find(tail)
-              case Nil          => None
-          find(allNames)
+        find(allKeys)
+      def caseInsensitiveCheck(ut : Untemplate.AnyUntemplate) : Option[T] = check(ut.UntemplateAttributesLowerCased, lcs)
+      def caseSensitiveCheck(ut : Untemplate.AnyUntemplate) : Option[T] = check(ut.UntemplateAttributes, allNames)
   enum Key[T]( converter : Key.Converter[T], variations : List[String] ) extends Key.Abstract[T](converter, variations):
     case `Title`              extends Key[String]                           (Key.Converter.SimpleString,                      Nil)
     case `Author`             extends Key[List[String]]                     (Key.Converter.StringList,   "Authors"         :: Nil)
--- ztapir/src/unstatic/ztapir/simple/exception.scala
+++ ztapir/src/unstatic/ztapir/simple/exception.scala
@@ -3,3 +3,4 @@ package unstatic.ztapir.simple
 import unstatic.UnstaticException
 
 class NoHtmlifierForContentType( msg : String, cause : Throwable = null ) extends UnstaticException(msg, null)
+class BadAttributeException( msg : String, cause : Throwable = null ) extends UnstaticException(msg, null)
